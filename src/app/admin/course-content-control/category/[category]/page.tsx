04456218f605445757a6cbfcf3565cf27304c2d3
siyammd553-gif <siyammd553-gif@users.noreply.github.com>
2026-08-25 20:50:41 +0000

    lint: suppress set-state-in-effect in category courses loader

--- src/app/admin/course-content-control/category/[category]/page.tsx
+++ src/app/admin/course-content-control/category/[category]/page.tsx
@@ -53,6 +53,7 @@ export default function CategoryCoursesPage({
   }, [category]);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     void load();
   }, [load]);
 
